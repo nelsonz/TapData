617f35544c66570f9d8eb0d72f7aae0219435e49
DerivativeDrivel <nelsonz@acm.org>
2012-07-22 01:00:00 -0700

    arduino server

--- whack-node/server.js
+++ whack-node/server.js
@@ -1,10 +1,28 @@
+var serial = require('serialport'),
+	WebSocketServer = require('ws').Server;
 
+var port = process.argv[2],
+	prev = "";
+	saving = false,
+	ws;
+	wss = new WebSocketServer({port: 9000});
+	
+wss.on('connection', function(socket) {
+	ws = socket;
+});
 
-var WebSocketServer = require('ws').Server
-var wss = new WebSocketServer({port: 9000});
-wss.on('connection', function(ws) {
-  ws.send('test');
-  ws.on('message', function(message) {
-    
-  });
+wss.on('save', function(socket) {
+	saving = true;
+});
+
+serialPort = new serial.SerialPort(port, {
+	parser: serial.parsers.readline('\n')
+});
+
+serialPort.on("data", function(data) {
+	if(ws && (saving || prev != data)) {
+		ws.send(data);
+		prev = data;
+		saving = false;
+	}
 });
